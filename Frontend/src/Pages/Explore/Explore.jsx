24e8f5e62854111aad3ba9edd8874e951967c313
Pasindu Mallawarachchi <Imalsha_map_e23@engug.ruh.ac.lk>
2024-08-31 15:02:17 +0530

    Explore Page card added

--- Frontend/src/Pages/Explore/Explore.jsx
+++ Frontend/src/Pages/Explore/Explore.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import TripCard from '../../Components/UI/TripCard'
 
 const Explore = () => {
   return (
-   <div className="w-full h-screen flex items-center justify-center  relative z-[-1]">Explore</div>
+   <div className="w-full h-screen flex items-center justify-center  relative z-[-1]">
+    <TripCard />
+   </div>
   )
 }
 
