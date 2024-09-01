73f31f2c8a7fc1639ac4ab5d6058cff2447f31bc
Isuru Sanjeewa Madusanka <143842041+ISMadusanka@users.noreply.github.com>
2024-09-02 01:18:48 +0530

    auth provider aded

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,11 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import AuthProvider from "./Contexts/Authprovider.jsx";
 
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
