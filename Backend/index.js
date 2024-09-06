4c73aa82f95b7553b295c2156f284a11414129dc
Pasindu Mallawarachchi <Imalsha_map_e23@engug.ruh.ac.lk>
2024-09-06 12:50:32 +0530

    db

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ app.use(
 );
 app.use(express.json());
 
-app.use(
+app.use( 
   session({
     secret: process.env.SESSION_SECRET || "secretKey",
     resave: false,
@@ -30,24 +30,27 @@ app.use(
     cookie: { maxAge: 24 * 60 * 60 * 1000 },
   })
 );
-
+ 
 app.use(passport.initialize());
 app.use(passport.session());
-
+  
 app.use("/auth", authRouter);
 app.use("/packages", packageRouter);
 app.use("/payment", paymentRouter);
 
-// Connect to MongoDB
+
+// Connect to MongoDB 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { 
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
-});
+}); 
+ 
+
